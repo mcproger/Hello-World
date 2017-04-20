2786802b6fc8106f39043e424eb5c5e58019e748
mcproger <artem7lomonosov@gmail.com>
2017-04-20 20:02:23 +0300

    Update api_tmdb.py

--- api_tmdb.py
+++ api_tmdb.py
@@ -1,7 +1,13 @@
-from helpers import load_json_data_from_url
-from helpers import make_tmdb_api_request
-from helpers import movie_budget
-from helpers import get_user_api_key
+import unittest
+from tmdb_api_helpers import load_json_data_from_url
+from tmdb_api_helpers import make_tmdb_api_request
+from tmdb_api_helpers import enter_api_key
+
+
+def get_saw_film_budget(api_key):
+    return make_tmdb_api_request(method='/movie/215', api_key=api_key, extra_params=None)['budget']
+
+
 if __name__ == '__main__':
-    user_api_key = get_user_api_key()
-    print(movie_budget(user_api_key))
+    api_key = enter_api_key()
+ 	print(get_saw_film_budget(api_key))
